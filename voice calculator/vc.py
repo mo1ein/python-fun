c1b7dd47eee0afb55ae858bdf42577b6b762e94f
Rudransh7171 <58659907+Rudransh7171@users.noreply.github.com>
2020-09-02 12:10:50 +0530

    Update vc.py

--- voice calculator/vc.py
+++ voice calculator/vc.py
@@ -1,35 +1,53 @@
 import speech_recognition as sr
-st = sr.Recognizer()
-with sr.Microphone() as source:
-    audio = st.listen(source)
-    try:
-        text = st.recognize_google(audio)
-        a = text.split()
-        for n,i in enumerate(a):
-            if i == 'thousand':
-                a[n] = 1000
-        for n,i in enumerate(a):
-            if i == 'hundred':
-                a[n] = 100
-        if a[1] == '+':
-            print(int(a[0])+int(a[2]))
-        elif a[1] == 'plus':
-            print(int(a[0])+int(a[2]))
-        elif a[1] == '-':
-            print(int(a[0])-int(a[2]))
-        elif a[1] == 'minus':
-            print(int(a[0])-int(a[2]))
-        elif a[1] == 'divided':
-            print(int(a[0])/int(a[-1]))
-        elif a[1] == '*':
-            print(int(a[0])*int(a[2]))
-        elif a[1] == 'into':
-            print(int(a[0])*int(a[2]))
-        elif a[1] == 'multiply':
-            print(int(a[0])*int(a[2]))
-        elif a[1] == 'in':
-            print(int(a[0])*int(a[2]))
-        else:
-            print('cant understand')
-    except:
-        print('Speak again!!')
+from tkinter import *
+parent_calculator =Tk()
+parent_calculator.title('Voice Calculator')
+parent_calculator.geometry('500x500')
+
+def voicec():
+    st = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = st.listen(source)
+        try:
+            text = st.recognize_google(audio)
+            a = text.split()
+            for n,i in enumerate(a):
+                if i == 'thousand':
+                    a[n] = 1000
+            for n,i in enumerate(a):
+                if i == 'hundred':
+                    a[n] = 100
+            if a[1] == '+':
+                label1 = Label(parent_calculator, text =  int(a[0])+int(a[2]))
+                label1.pack()
+            elif a[1] == 'plus':
+                label1 = Label(parent_calculator, text =  int(a[0])+int(a[2]))
+                label1.pack()
+            elif a[1] == '-':
+                label1 = Label(parent_calculator, text = int(a[0])-int(a[2]))
+                label1.pack()
+            elif a[1] == 'minus':
+                label1 = Label(parent_calculator,text = int(a[0])-int(a[2]))
+                label1.pack()
+            elif a[1] == 'divided':
+                label1 = Label(parent_calculator,text = int(a[0])/int(a[-1]))
+                label1.pack()
+            elif a[1] == '*':
+                label1 = Label(parent_calculator,text = int(a[0])*int(a[2]))
+                label1.pack()
+            elif a[1] == 'into':
+                label1 = Label(parent_calculator,text = int(a[0])*int(a[2]))
+                label1.pack()
+            elif a[1] == 'multiply':
+                label1 = Label(parent_calculator,text = int(a[0])*int(a[2]))
+                label1.pack()
+            elif a[1] == 'in':
+                label1 = Label(parent_calculator,text = int(a[0])*int(a[2]))
+                label1.pack()
+            else:
+                print('cant understand')
+        except:
+            print('Speak again!!')
+button = Button(parent_calculator, text = 'tap to speak', command = voicec)
+button.pack()
+parent_calculator.mainloop()
